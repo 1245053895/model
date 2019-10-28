c542d471b2d1e5434a93a1caf8e7bb633c604e8d
AnnualSalary100W <1697225591@qq.com>
2019-10-28 17:28:38 +0800

    wang

--- src/main/java/com/xd/zt/controller/experiment/ExperimentController.java
+++ src/main/java/com/xd/zt/controller/experiment/ExperimentController.java
@@ -1,14 +1,22 @@
 package com.xd.zt.controller.experiment;
 
 
+import com.xd.zt.service.experiment.ExperimentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("/experiment")
 public class ExperimentController {
+    @Autowired
+    private ExperimentService experimentService;
+
+
     @RequestMapping("/index")
     public ModelAndView experimentindex(Model model) {
         return new ModelAndView("experiment/index", "modelModel", model);
@@ -21,4 +29,21 @@ public class ExperimentController {
     public ModelAndView datalead(Model model) {
         return new ModelAndView("experiment/datalead", "modelModel", model);
     }
+
+
+    /*@RequestMapping("/modelConfiguration")
+    public ModelAndView modelConfiguration(Model model) {
+        return new ModelAndView("experiment/modelConfiguration", "modelModel", model);
+    }*/
+    @RequestMapping("/modelConfiguration")
+    public ModelAndView modelConfiguration(Model model){
+
+        ArrayList modelConfiguration  = experimentService.modelConfiguration();
+
+        model.addAttribute("modelConfiguration",modelConfiguration);
+
+        return new ModelAndView("experiment/modelConfiguration","Modelmodel",model) ;
+    }
+
+
 }
--- src/main/java/com/xd/zt/serviceImpl/experiment/experimentServiceImp.java
+++ src/main/java/com/xd/zt/serviceImpl/experiment/experimentServiceImp.java
@@ -1,4 +1,19 @@
 package com.xd.zt.serviceImpl.experiment;
 
-public class experimentServiceImp {
+import com.xd.zt.mapper.experiment.ExperimentMapper;
+import com.xd.zt.service.experiment.ExperimentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class experimentServiceImp implements ExperimentService {
+
+    @Autowired
+    private ExperimentMapper experimentMapper;
+
+    @Override
+    public ArrayList modelConfiguration() {
+        return experimentMapper.modelConfiguration();
+    }
 }
