91bb805e1b28c44256c1d71d68e1ec6ec7d0deff
15002994538 <15002994538@163.com>
2019-11-18 09:36:03 +0800

    11.18Echarts--张立军

---
+++ src/main/java/com/xd/zt/controller/echarts/TzgsEchartsController.java
@@ -0,0 +1,14 @@
+package com.xd.zt.controller.echarts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/echarts")
+@Controller
+public class TzgsEchartsController {
+
+    @RequestMapping("/Tzgs")
+    public String Tzgs(){
+        return "echarts/Tzgs";
+    }
+}
